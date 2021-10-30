fbb4fe86896e3f15cecab6d236d6fe8561940112
Jeff Hutchison <jeff@jeffhutchison.com>
2021-10-30 18:03:14 -0400

    Initial commit 🚀

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Zinc zrepl console web application";
+
+  inputs = {
+    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    poetry2nix = {
+      url = github:nix-community/poetry2nix;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    {
+      # Nixpkgs overlay providing the application
+      overlay = nixpkgs.lib.composeManyExtensions [
+        poetry2nix.overlay
+        (final: prev: {
+          # The application
+          zinc = prev.poetry2nix.mkPoetryApplication {
+            projectDir = ./.;
+          };
+        })
+      ];
+    } // (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        };
+      in
+      rec {
+        packages = {
+          zinc = pkgs.zinc;
+        };
+
+        # defaultApp = apps.zinc;
+        defaultPackage = packages.zinc;
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ python3 poetry ];
+          shellHook = ''
+            export FLASK_APP=zinc
+            export FLASK_ENV=development
+          '';
+        };
+      }));
+}
