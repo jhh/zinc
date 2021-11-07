9157a4968d0a2a296fce90d0cfcaeeba9069d3e3
Jeff Hutchison <jeff@jeffhutchison.com>
2021-11-07 13:34:43 -0500

    Added error output

--- zinc/api.py
+++ zinc/api.py
@@ -9,10 +9,14 @@ bp = Blueprint("api", __name__, url_prefix="/api")
 @bp.route("/status")
 def status():
     zrepl = os.environ["ZREPL"]
-    out = subprocess.run(
-        ["sudo", zrepl, "status", "--mode=raw"],
-        check=True,
-        capture_output=True,
-        text=True,
-    )
-    return Response(out.stdout, mimetype="application/json")
+    try:
+        out = subprocess.run(
+            ["sudo", zrepl, "status", "--mode=raw"],
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+        return Response(out.stdout, mimetype="application/json")
+    except subprocess.CalledProcessError as err:
+        print(f"stdout={err.stdout}\nstderr={err.stderr}")
+        return None
