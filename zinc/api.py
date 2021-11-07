7757d5e75368bedc7db1f3017961942bbddc3109
Jeff Hutchison <jeff@jeffhutchison.com>
2021-11-07 13:14:52 -0500

    Pass in zrepl path via env

--- zinc/api.py
+++ zinc/api.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from flask import Blueprint
 from flask.wrappers import Response
@@ -7,7 +8,8 @@ bp = Blueprint("api", __name__, url_prefix="/api")
 
 @bp.route("/status")
 def status():
+    zrepl = os.environ["ZREPL"]
     out = subprocess.run(
-        ["zrepl", "status", "--mode=raw"], capture_output=True, text=True
+        [zrepl, "status", "--mode=raw"], capture_output=True, text=True
     )
     return Response(out.stdout, mimetype="application/json")
